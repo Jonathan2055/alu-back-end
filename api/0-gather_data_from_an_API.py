dd7706b8db430ce24fcb9a36b4ab289c4a45a879
Jonathan2055 <j.mugisha3@alustudent.com>
2025-07-08 12:03:24 +0000

    Debugged to by pass pip 8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,18 +6,26 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
     employee_id = int(sys.argv[1])
 
     # Get user info
-    user_url = "https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("Employee not found.")
+        sys.exit(1)
+
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
     # Get user's TODOs
     todos_url = (
         "https://jsonplaceholder.typicode.com/todos"
-f"?userId={employee_id}"
+        f"?userId={employee_id}"
     )
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
